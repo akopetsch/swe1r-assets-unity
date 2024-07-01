505f7fa9bbb3ed4bb8d8640701f6887f3bed6a36
Alexander Kopetsch <akopetsch92@gmail.com>
2024-07-01 17:08:43 +0200

    dummy spline importer/exporter

---
+++ Samples~/SampleProject/Assets/Code/SplineBlock/SplineBlockItemExporter.cs
@@ -0,0 +1,11 @@
+﻿// SPDX-License-Identifier: MIT
+
+using SWE1R.Assets.Blocks.SplineBlock;
+
+namespace SWE1R.Assets.Blocks.Unity.SplineBlock
+{
+    public class SplineBlockItemExporter : BlockItemExporter<SplineBlockItem>
+    {
+
+    }
+}
---
+++ Samples~/SampleProject/Assets/Code/SplineBlock/SplineBlockItemImporter.cs
@@ -0,0 +1,11 @@
+﻿// SPDX-License-Identifier: MIT
+
+using SWE1R.Assets.Blocks.SplineBlock;
+
+namespace SWE1R.Assets.Blocks.Unity.SplineBlock
+{
+    public class SplineBlockItemImporter : BlockItemImporter<SplineBlockItem>
+    {
+
+    }
+}
